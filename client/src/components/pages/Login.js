6fe1f5d29793efd675f6be63b29ae4de29802c98
Jason Wisniewski <jwizinpa95@gmail.com>
2022-03-30 15:39:43 -0600

    user post form functioning

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -4,24 +4,42 @@ import { validateEmail } from '../../utils/helpers';
 
 const Login = () => {
   
-  // const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
-  // const [errorMessage, setErrorMessage] = useState('');
-  // const { name, email, message } = formState;
-  // const handleChange = (e) => {
-  //   if (e.target.name === 'email') {
-  //     const isValid = validateEmail(e.target.value);
-  //     if(!isValid) {
-  //       setErrorMessage('Your email is invalid.');
-  //     } else {
-  //       setErrorMessage('');
-  //     }
-  //   }
-  // }
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+  
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if(!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    }
+  }
+
+  const login = () => {
+    console.log('login click')
+  };
+
+  const signUpHere = () => {
+    console.log('signUpHere click')
+  };
+
+  
 
   return (
     <section className="d-flex justify-content-around align-items-center p-5 bg-primary height-100">
-      <form className="card p-5" id ="contact-form red">
+      <form className="card p-5" id="logInForm" onSubmit ={handleSubmit}>
         <h1 className= "text-center pb-4" data-testid='h1tag'>Log in</h1>
         
         <label className="p-2" htmlFor="email">Email address :</label>
@@ -30,9 +48,9 @@ const Login = () => {
         <label className="p-2" htmlFor="name">Password :</label>
         <input className="m-2" type="text" name="name"/>
 
-        <button className="btn btn-primary m-2">Login</button>
+        <button className="btn btn-primary m-2" onClick={login} >Login</button>
 
-        <button className="btn btn-warning m-2"> sign up here</button>
+        <button className="btn btn-warning m-2" onClick={signUpHere}>sign up here</button>
       </form>
       
 
