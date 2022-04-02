e6be440d6c0bde191d0a277dea2c868e4e012a97
Jason Wisniewski <jwizinpa95@gmail.com>
2022-04-01 22:11:12 -0600

    footer fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import Container from './components/Container';
-
+import React from "react";
+import Container from "./components/Container";
 function App() {
   return (
     <>
