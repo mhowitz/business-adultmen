a44d634f8d3841abed385875ced83f876796936d
David "Smoom" Tait <david.tait.96@gmail.com>
2022-04-01 21:41:52 -0600

    modal hovers and centered

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,10 +4,10 @@ function Modal({ onClose, currentPhoto }) {
 
   return (
     <div className="modalBackdrop">
-      <div className="modalContainer">
+      <div className="modalContainer d-flex flex-column align-items-center justify-content-center">
         {/* <h3 className="modalTitle">{name}</h3> */}
         <img src={currentPhoto} alt="current category" />
-        <button type="button" onClick={onClose}>
+        <button className="modalButton" type="button" onClick={onClose}>
           Close this modal
         </button>
       </div>
