335880269ab599051012d5213ef8064437c3544d
Jason Wisniewski <jwizinpa95@gmail.com>
2022-04-03 13:34:08 -0600

    log in half working

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
