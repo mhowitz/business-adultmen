0b71adbadc50ea597b1403fc9276087307d08d3d
mikwitz <mikaylahowitz@gmail.com>
2022-04-06 12:08:31 -0600

    changing routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const jsonwebtoken = require('jsonwebtoken');
-const { authMiddleware } = require('./utils/auth');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 // Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
 const apiRoutes = require('./api');
-
+const path = require('path')
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+router.get('*', (rerq, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+} );
+
+// router.use((req, res) => {
+//   res.status(404).send('<h1>😝 404 Error!</h1>');
+// });
 
 module.exports = router;
