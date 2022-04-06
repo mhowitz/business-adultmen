fab611d69277708cf9b86c5da9bb28fe5560c059
mikwitz <mikaylahowitz@gmail.com>
2022-04-06 12:25:40 -0600

    heroku fixes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,9 @@ const path = require('path')
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
 
-router.get('*', (rerq, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'))
-} );
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+// } );
 
 // router.use((req, res) => {
 //   res.status(404).send('<h1>😝 404 Error!</h1>');
