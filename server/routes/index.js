5ab9d63f2c079826ceb1f8f735fc0f20d09b22e1
mikwitz <mikaylahowitz@gmail.com>
2022-04-06 12:11:08 -0600

    heroku fixes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 router.use('/api', apiRoutes);
 
 router.get('*', (rerq, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  res.sendFile(path.join(__dirname, '../client/build'))
 } );
 
 // router.use((req, res) => {
