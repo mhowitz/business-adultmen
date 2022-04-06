b6c45f564721df737cb895ec36763a94259758be
mikwitz <mikaylahowitz@gmail.com>
2022-04-05 20:42:54 -0600

    path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const jsonwebtoken = require('jsonwebtoken');
